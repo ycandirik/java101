feac03800e3214d687336cd80056539e6725a83c
ycandirik <yunuscandirik91@gmail.com>
2022-04-05 16:26:13 +0300

    atm projesi

---
+++ whileDongusu.java
@@ -0,0 +1,21 @@
+
+public class whileDongusu {
+    public static void main(String[] args) {
+        int i = 1;
+        int k;
+
+        while (i <= 5){
+            System.out.println(i);
+            i++;
+            k = 1;
+            while(k <= 10) {
+                System.out.print(k + " , ");
+                k++;
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
